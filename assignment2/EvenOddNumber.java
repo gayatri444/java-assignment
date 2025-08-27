f5a2ce03844bdf77cd9c8b64f629f5a8495015fe
gayatri444 <gayatriraykar09@gmail.com>
2025-08-27 22:31:22 +0530

    code

---
+++ assignment2/EvenOddNumber.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class EvenOddNumber{
+	public static void main(String args[]){
+	int n;
+	Scanner sc=new Scanner(System.in);
+	System.out.println("Enter a Number");
+	n=sc.nextInt();
+	if(n%2==0)
+	System.out.println("Number is even");
+	else
+	System.out.println("Number is odd");
+ }
+}
